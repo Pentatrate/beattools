564b6e17d084dad1170450fbfcea6ab08d1d949d
Pentatrate <190233437+Pentatrate@users.noreply.github.com>
2026-04-26 13:07:45 +0200

    3.8.4 Another imgui fix attempt

--- files/bookmarkList.lua
+++ files/bookmarkList.lua
@@ -3,7 +3,7 @@ return function(window_flag, inputFlag)
 		helpers.SetNextWindowPos(750, 420, window_flag)
 		helpers.SetNextWindowSize(200, 300, window_flag)
 		mod.config.bookmarkList = imgui.Begin("Bookmarks", true, (inputFlag or 0) + (mod.config.stopImGuiMove and imgui.ImGuiWindowFlags_NoMove or 0) + (mod.config.stopImGuiResize and imgui.ImGuiWindowFlags_NoResize or 0))
-		if utilitools.files.beattools.easing.list.bookmark and utilitools.files.beattools.easing.list.bookmark["_"] and utilitools.files.beattools.easing.list.bookmark["_"]["_"] then
+		if utilitools.files.beattools.easing.list.bookmark and utilitools.files.beattools.easing.list.bookmark["_"] and utilitools.files.beattools.easing.list.bookmark["_"]["_"] and #utilitools.files.beattools.easing.list.bookmark["_"]["_"] > 0 then
 			local _, count = utilitools.files.beattools.easing.getEase("bookmark", nil, cs.editorBeat, nil, nil)
 			for i, bookmark in ipairs(utilitools.files.beattools.easing.list.bookmark["_"]["_"]) do
 				if i ~= 1 then imgui.Separator() end
